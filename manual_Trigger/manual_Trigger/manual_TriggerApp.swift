e1777ae5b63d0f48e0b9a0ba095635b91daf514e
abdul <abdul_ahad1996@live.com>
2025-11-07 23:28:51 +0100

    wip

---
+++ manual_Trigger/manual_Trigger/manual_TriggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  manual_TriggerApp.swift
+//  manual_Trigger
+//
+//  Created by Abdul Ahad on 07.11.25.
+//
+
+import SwiftUI
+
+@main
+struct manual_TriggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ manual_Trigger/manual_TriggerTests/manual_TriggerTests.swift
@@ -0,0 +1,17 @@
+//
+//  manual_TriggerTests.swift
+//  manual_TriggerTests
+//
+//  Created by Abdul Ahad on 07.11.25.
+//
+
+import Testing
+@testable import manual_Trigger
+
+struct manual_TriggerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

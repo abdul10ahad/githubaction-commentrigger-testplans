ef2236949478aa259923a72ad31588d5245abd13
abdul <abdul_ahad1996@live.com>
2025-11-08 00:54:49 +0100

    Test: Add text to demonstrate test plans

--- manual_Trigger/manual_Trigger/ContentView.swift
+++ manual_Trigger/manual_Trigger/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("Testing comment-triggered workflow")
+            Text("With test plans!")
         }
         .padding()
     }

d9980fda6e8379519b315c8bcf69bddc6982baa8
abdul <abdul_ahad1996@live.com>
2025-11-09 23:52:21 +0100

    wip

--- Features/MathFeature/Sources/MathHelper.swift
+++ Features/MathFeature/Sources/MathHelper.swift
@@ -27,5 +27,11 @@ public struct MathHelper {
         return a / b
     }
 
+    // New function: modulo operation
+    public static func modulo(_ a: Int, _ b: Int) -> Int {
+        guard b != 0 else { return 0 }
+        return a % b
+    }
+
     public init() {}
 }
